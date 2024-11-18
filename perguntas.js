89327645d323507a9f23f5dc2f631e11eacff191
nerijola <142917441+nerijola@users.noreply.github.com>
2024-11-18 11:44:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Futebol',
+    'Quem é o rei do futebol?',
+    'O rei do futebol é Pelé'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual continente se localiza o Vaticano?',
+    'Se localiza no continente Europeu'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o top 5 times do brasil com mais titulos?',
+    '1 Palmeiras,  2 Flamengo,  3 São Paulo,  4 Santos,  5 Cruzeiro'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual time revelou o Rei do futebol?',
+    'Santos Futebol Clube'
+)
+
+criaCartao(
+    'VideoGame',
+    'Quando foi criado o primeiro VideoGame da história?',
+    'Desenvolvido entre 1966 e 1968, mas lançado somente em 1972'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o maior time do mundo?',
+    'Real Madrid (Espanha)'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o menor time do mundo?',
+    'Íbis Sport Club'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano o Brasil ganhou sua primeira copa do mundo?',
+    '1958'
+)
